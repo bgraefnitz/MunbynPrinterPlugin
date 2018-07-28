86122929b4fab2bba55631d969465e98e6b0bac1
bgraefnitz <36892307+bgraefnitz@users.noreply.github.com>
2018-07-28 01:02:20 -0400

    ios updates

--- src/iOS/MunbynWrapper.m
+++ src/iOS/MunbynWrapper.m
@@ -1,14 +1,37 @@
 #import "MunbynWrapper.h"
 #import <Cordova/CDVPlugin.h>
+#import "PrinterSDK.h"
 
 @implementation MunbynWrapper
 
 - (void)write:(CDVInvokedUrlCommand*)command
 {
+    NSArray * arguments = [command arguments];
+    NSString *printerName = [arguments objectAtIndex:0];
+    NSString *printString = [arguments objectAtIndex:1];
+    
     CDVPluginResult* pluginResult = nil;
     NSString* message = [command.arguments objectAtIndex:0];
-
-    if (message != nil && [message length] > 0) {
+    __block NSMutableArray* _printerArray;
+    if (printString != nil && printerName != nil) {
+        [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer* printer)
+         {
+             NSLog(@"win");
+             if (nil == _printerArray)
+             {
+                 _printerArray = [[NSMutableArray alloc] initWithCapacity:1];
+             }
+             
+             [_printerArray addObject:printer];
+         }];
+        [[PrinterSDK defaultPrinterSDK] scanPrintersWithCompletion:^(Printer *printer)
+          {
+              if([printer.name isEqualToString:@"BlueTooth Printer"])
+              {
+                  [[PrinterSDK defaultPrinterSDK] connectBT:printer];
+                  [[PrinterSDK defaultPrinterSDK] printText:@"testing iOS"];
+              }
+          }];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
